65ee3a9e7c6341e1becb4a8202bd74d3b84f3b7b
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2013-03-18 17:05:58 -0700

    WriteData

---
+++ WriteData.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class WriteData
+{
+  public static void main(String[]args) throws IOException
+  {
+    File file = new File ("girls");
+    if(file.exists())
+    {
+      System.out.println("File already exists");
+      System.exit(1);
+    }
+
+    PrintWriter output = new PrintWriter(file);
+
+    output.println("Hello world");
+    output.println("Hello Earth");
+    output.println("Hello Sun");
+    output.println("Hello Moon");
+    
+    output.close();
+  }
+}
